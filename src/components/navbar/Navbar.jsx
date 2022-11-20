c958bdcc9e634b88bdb24569bd4f6d06512c66ff
Mateusz <wosmateusz2@gmail.comm>
2022-11-20 10:04:20 +0100

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { Fragment, useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../assets/logo.svg';
+
+const Menu = () => {
+	return (
+		<Fragment>
+			<p>
+				<a href='#home'>Home</a>
+			</p>
+			<p>
+				<a href='#wgpt3'>What is GPT?</a>
+			</p>
+			<p>
+				<a href='#possibility'>Open AI</a>
+			</p>
+			<p>
+				<a href='#features'>Case Studies</a>
+			</p>
+			<p>
+				<a href='#blog'>Library</a>
+			</p>
+		</Fragment>
+	);
+};
+
+const Navbar = () => {
+	const [toggleMenu, settoggleMenu] = useState(false);
+
+	const menuCloseHandler = () => {
+		settoggleMenu(false);
+	};
+	const menuOpenHandler = () => {
+		settoggleMenu(true);
+	};
+	return (
+		<div className='gpt3__navbar'>
+			<div className='gpt3__navbar-links'>
+				<div className='gpt3__navbar-links_logo'>
+					<img src={logo} alt='logo' />
+				</div>
+				<div className='gpt3__navbar-links_container'>
+					<Menu />
+				</div>
+				<div className='gpt3__navbar-sign'>
+					<p>Sign in</p>
+					<button type='button'>Sign up</button>
+				</div>
+				<div className='gpt3__navbar-menu'>
+					{toggleMenu ? (
+						<RiCloseLine color='#fff' size={27} onClick={menuCloseHandler} />
+					) : (
+						<RiMenu3Line color='#fff' size={27} onClick={menuOpenHandler} />
+					)}
+					{toggleMenu && (
+						<div className='gpt3__navbar-menu_container scale-up-center'>
+							<div className='gpt3__navbar-menu_container-links'>
+								<Menu />
+							</div>
+							<div className='gpt3__navbar-menu_container-links-sign'>
+								<p>Sign in</p>
+								<button type='button'>Sign up</button>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
